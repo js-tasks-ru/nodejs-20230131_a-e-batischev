1de327d134ceb5c509ae8b2d80062fcddc139e92
Andrey <a.e.batischev@gmail.com>
2023-02-23 09:56:05 +0300

    3-module 3-task правки

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -9,23 +9,21 @@ const server = new http.Server()
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`)
   const pathname = url.pathname.slice(1)
-
   const filepath = path.join(__dirname, 'files', pathname)
   const stream = fs.createReadStream(filepath)
 
+  stream.on('error', (e) => {
+    if (pathname.includes('/')) {
+      res.statusCode = 400
+      res.end('Nested folders are not supported')
+    } else if (!fs.existsSync(filepath)) {
+      res.statusCode = 404
+      res.end('No such file')
+    }
+  })
+
   switch (req.method) {
     case 'GET':
-      stream.on('error', (error) => {
-        if (error.code === 'ENOENT') {
-          if (error.path.slice(error.path.indexOf('files')).split('\\').length > 2) {
-            res.statusCode = 400;
-            res.end('Nested folders are not supported');
-          } else if (!fs.existsSync(filepath)) {
-            res.statusCode = 404
-            res.end();
-          }
-        }
-      })
 
       stream.on('data', chunk => {
         res.write(chunk)
