b180c23fd66af6400c7bd23e0a1114d27c660210
Andrey <a.e.batischev@gmail.com>
2023-03-12 20:52:07 +0300

    7-module 1-task ready

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,17 +1,26 @@
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../../models/User')
 
-module.exports = new LocalStrategy(
-  { usernameField: 'email', session: false },
-  function (email, password, done) {
+module.exports = new LocalStrategy({ usernameField: 'email', session: false },
 
-    User.findOne({ email: email }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false, 'Нет такого пользователя'); }
-      if (!user.checkPassword(password)) { return done(null, false); }
+  async function (email, password, done) {
+    try {
+      const user = await User.findOne({ email: email })
+      if (!user) {
+        return done(null, false, 'Нет такого пользователя');
+      }
+
+      const isPasswordValid = await user.checkPassword(password)
+
+      if (!isPasswordValid) {
+        return done(null, false, 'Неверный пароль');
+      }
 
       return done(null, user);
-    });
 
+
+    } catch (error) {
+      return done(error)
+    }
   },
 );
