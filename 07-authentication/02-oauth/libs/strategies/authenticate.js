83b0732837f6ded5d6916163482c37164d0f9f17
Andrey <a.e.batischev@gmail.com>
2023-03-18 08:02:54 +0300

    7 module 3 task

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,24 +1,15 @@
 const User = require('../../models/User')
-
-
 module.exports = async function authenticate(strategy, email, displayName, done) {
-
   try {
-    console.log(1);
     if (!email) return done(null, false, 'Не указан email')
-
     const user = await User.findOne({ email: email })
-
     if (!user) {
-      console.log('email', email);
       const newUser = await User.create({ email, displayName })
       done(null, newUser)
     } else {
       done(null, user)
     }
   } catch (error) {
-    console.log(2);
-    // console.log(error);
     done(error, false, 'Некорректный email.')
   }
 };
---
+++ 07-authentication/03-sessions/libs/mustBeAuthenticated.js
@@ -0,0 +1,6 @@
+module.exports = function mustBeAuthenticated(ctx, next) {
+  if (!ctx.user) {
+    ctx.throw(401, 'Пользователь не залогинен')
+  }
+  return next();
+};
