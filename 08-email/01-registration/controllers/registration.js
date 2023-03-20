70f559993be3fbc76aff4bf73607ea76b6a1783a
Andrey <a.e.batischev@gmail.com>
2023-03-20 07:52:30 +0300

    8-module 1 task

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -6,34 +6,27 @@ module.exports.register = async (ctx, next) => {
     const verificationToken = uuid()
     const { email, displayName, password } = ctx.request.body;
 
-    const user = await User.findOne({ email: email });
+    const newUser = new User({ email, displayName, verificationToken });
 
-    if (!user) {
-        console.log('create new user');
-        await User.create({ email, displayName, password, verificationToken });
-        const mails = await sendMail({
-            template: 'confirmation',
-            locals: { token: verificationToken },
-            to: email,
-            subject: 'Подтвердите почту',
-        });
-        console.log(mails);
+    await newUser.setPassword(password)
+    await newUser.save()
 
-        ctx.body = { status: 'ok' };
+    await sendMail({
+        template: 'confirmation',
+        locals: { token: verificationToken },
+        to: email,
+        subject: 'Подтвердите почту',
+    });
 
-    } else {
-        console.log('user allready exyst');
-        ctx.body = { errors: { email: 'Такой email уже существует' } }
-    }
+
+    ctx.body = { status: 'ok' };
+
 };
 
 module.exports.confirm = async (ctx, next) => {
     const user = await User.findOne({
         verificationToken: ctx.request.body.verificationToken,
     });
-    console.log(
-        ctx.request.body.verificationToken
-    );
 
     if (!user) {
         ctx.throw(400, 'Ссылка подтверждения недействительна или устарела');
@@ -45,5 +38,6 @@ module.exports.confirm = async (ctx, next) => {
     const token = uuid();
 
     ctx.body = { token };
+
 
 };
